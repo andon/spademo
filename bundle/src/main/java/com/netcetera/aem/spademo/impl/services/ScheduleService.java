19486e121455718a37668da0269be4066a76a117
Andon Sikavica <andon.sikavica@netcetera.com>
2014-09-17 18:38:22 +0200

    Minor corrections and clean up

--- bundle/src/main/java/com/netcetera/aem/spademo/impl/services/ScheduleService.java
+++ bundle/src/main/java/com/netcetera/aem/spademo/impl/services/ScheduleService.java
@@ -41,32 +41,23 @@ public class ScheduleService {
     Sort sort = sortingInfo.getSort();
     SortDirection sortDirection = sortingInfo.getSortDirection();
 
-    int[] numbers = new int[]{1, 2, 3, 4};
+    int[] numbers = new int[]{1, 2, 3, 4, 5};
     switch (day) {
       case 1:
         List<Slot> orderedFirstDaySlots = new ArrayList<Slot>(FIRST_DAY_SLOTS);
         Collections.sort(orderedFirstDaySlots, sort.getComparator(sortDirection));
-        return new ConferenceDay(
-            new LocalDate(2014, 9, 22), 
-            orderedFirstDaySlots, 
-            sortingInfo, 
-            new PagingInfo(numbers, 1, true, false));
+        return new ConferenceDay(new LocalDate(2014, 9, 22), orderedFirstDaySlots, sortingInfo, new PagingInfo(numbers,
+            1, true, false));
       case 2:
         List<Slot> orderedSecondDaySlots = new ArrayList<Slot>(SECOND_DAY_SLOTS);
         Collections.sort(orderedSecondDaySlots, sort.getComparator(sortDirection));
-        return new ConferenceDay(
-            new LocalDate(2014, 9, 23), 
-            orderedSecondDaySlots, 
-            sortingInfo, 
-            new PagingInfo(numbers, 2, true, true));
+        return new ConferenceDay(new LocalDate(2014, 9, 23), orderedSecondDaySlots, sortingInfo, new PagingInfo(
+            numbers, 2, true, true));
       case 3:
         List<Slot> orderedThirdDaySlots = new ArrayList<Slot>(THIRD_DAY_SLOTS);
         Collections.sort(orderedThirdDaySlots, sort.getComparator(sortDirection));
-        return new ConferenceDay(
-            new LocalDate(2014, 9, 24), 
-            orderedThirdDaySlots, 
-            sortingInfo, 
-            new PagingInfo(numbers, 3, true, true));
+        return new ConferenceDay(new LocalDate(2014, 9, 24), orderedThirdDaySlots, sortingInfo, new PagingInfo(numbers,
+            3, true, true));
       case 4:
         throw new ConferenceException("Day 4 still in preparation");
       default:
@@ -79,9 +70,9 @@ public class ScheduleService {
     Set<Slot> firstDaySlots = new HashSet<Slot>();
     firstDaySlots.add(new Slot(new LocalTime(8, 30), new LocalTime(10, 30), "Rookie Session / AEM Certification",
         "Sebastian Schlick"));
-    firstDaySlots.add(new Slot(new LocalTime(10, 30), new LocalTime(10, 45), "Opening Words", "Dominik Süß"));
+    firstDaySlots.add(new Slot(new LocalTime(10, 30), new LocalTime(10, 45), "Opening Words", "Dominik S\u00fc\u00df"));
     firstDaySlots.add(new Slot(new LocalTime(10, 45), new LocalTime(11, 30), "Keynote: Apache Sling - Crystal Ball",
-        "Mike Müller, Carsten Ziegeler"));
+        "Mike M\u00fcller, Carsten Ziegeler"));
     firstDaySlots.add(new Slot(new LocalTime(11, 30), new LocalTime(11, 45), "Break", ""));
     firstDaySlots.add(new Slot(new LocalTime(11, 45), new LocalTime(12, 15), "Data replication in Sling",
         "Tommaso Teofili"));
@@ -93,10 +84,10 @@ public class ScheduleService {
         "David Bosschaert"));
     firstDaySlots.add(new Slot(new LocalTime(14, 30), new LocalTime(14, 45), "Break", ""));
     firstDaySlots.add(new Slot(new LocalTime(14, 45), new LocalTime(15, 30),
-        "Oak, the architecture of the new repository", "Michael Dürig"));
+        "Oak, the architecture of the new repository", "Michael D\u00fcrig"));
     firstDaySlots.add(new Slot(new LocalTime(15, 30), new LocalTime(15, 45), "Break", ""));
     firstDaySlots.add(new Slot(new LocalTime(15, 45), new LocalTime(16, 15),
-        "The proper use of ResourceAccessSecurity", "Mike Müller"));
+        "The proper use of ResourceAccessSecurity", "Mike M\u00fcller"));
     firstDaySlots.add(new Slot(new LocalTime(16, 15), new LocalTime(16, 30), "Break", ""));
     firstDaySlots.add(new Slot(new LocalTime(16, 30), new LocalTime(17, 0), "New features of the sling health check",
         "Georg Henzler"));
@@ -135,12 +126,13 @@ public class ScheduleService {
         "Dynamic Components using Single-Page-Application Concepts", "Andon Sikavica, Bojana Popovska"));
     thirdDaySlots.add(new Slot(new LocalTime(10, 30), new LocalTime(10, 45), "Break", ""));
     thirdDaySlots.add(new Slot(new LocalTime(10, 45), new LocalTime(11, 45), "Getting Started with iBeacons and AEM",
-        "Bartłomiej Soin"));
+        "Bart\u0142omiej Soin"));
     thirdDaySlots.add(new Slot(new LocalTime(11, 15), new LocalTime(11, 30), "Break", ""));
-    thirdDaySlots.add(new Slot(new LocalTime(11, 30), new LocalTime(12, 0), "The Sling JCRBrowser", "Sandro Böhme"));
+    thirdDaySlots.add(new Slot(new LocalTime(11, 30), new LocalTime(12, 0), "The Sling JCRBrowser",
+        "Sandro B\u00f6hme"));
     thirdDaySlots.add(new Slot(new LocalTime(12, 0), new LocalTime(13, 0), "Lunch", ""));
     thirdDaySlots.add(new Slot(new LocalTime(13, 0), new LocalTime(13, 30),
-        "OSGi Asynchronous Services: not just another RCP", "Michael Dürig"));
+        "OSGi Asynchronous Services: not just another RCP", "Michael D\u00fcrig"));
     thirdDaySlots.add(new Slot(new LocalTime(13, 30), new LocalTime(13, 45), "Break", ""));
     thirdDaySlots.add(new Slot(new LocalTime(13, 45), new LocalTime(14, 15),
         "Sling application architecture with Sling Models", "Stefan Seifert"));
